150c7ed14ad75a6b357b941be3e96d6f079f209a
Ezra Chiang <ezrachiang@Ezras-MacBook-Pro.local>
2016-12-20 23:43:11 -0800

    Add acknowledgements between client and server

--- public/js/index.js
+++ public/js/index.js
@@ -11,3 +11,10 @@ socket.on('disconnect', function () {
 socket.on('newMessage', function (message) {
   console.log('new message', message);
 });
+
+socket.emit('createMessage', {
+  from: "Charlie",
+  text: "Bark"
+}, function (data) {
+  console.log('Got it', data);
+});
